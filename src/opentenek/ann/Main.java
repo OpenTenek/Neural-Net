c4e2d1f088abea42f59cf1f24ac22b32bbad4545
Thinic <hamilton.nick.1@gmail.com>
2015-03-31 13:58:56 -0500

    add main class, add code style document

---
+++ src/opentenek/ann/Main.java
@@ -0,0 +1,19 @@
+package opentenek.ann;
+
+/* 
+ * File Name : Main.java
+ * Author(s) : Thinic
+ * Created   : 2015 Mar 31
+ * Updated   : 2015 Mar 31
+ * Version   : 0.0.1
+ * 
+ * This is where the application will be launched.
+ */
+
+public class Main
+{
+    public static void main(String args[]) 
+    {
+        
+    }
+}
---
+++ src/opentenek/ann/net/Neuron.java
@@ -0,0 +1,16 @@
+package opentenek.ann.net;
+
+/* 
+ * File Name : Neuron.java
+ * Author(s) : Thinic
+ * Created   : 2015 Mar 31
+ * Updated   : 2015 Mar 31
+ * Version   : 0.0.1
+ * 
+ * Basic unit for neural net.
+ */
+
+public class Neuron
+{
+    
+}
