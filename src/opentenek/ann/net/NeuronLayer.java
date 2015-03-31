bd58348ab2872aa20cb219387244c21900a84f31
Timberwolfx <classicguitar64@gmail.com>
2015-03-31 16:20:44 -0500

    yoyoyo

--- src/opentenek/ann/net/NeuronLayer.java
+++ src/opentenek/ann/net/NeuronLayer.java
@@ -2,7 +2,7 @@ package opentenek.ann.net;
 
 /* 
  * File Name : NeuronLayer.java
- * Author(s) : Ike
+ * Author(s) : Ike, Thinic
  * Created   : 2015 Mar 31
  * Updated   : 2015 Mar 31
  * Version   : 0.0.1
@@ -12,5 +12,37 @@ package opentenek.ann.net;
 
 public class NeuronLayer
 {
+    private Neuron layer[];
+    private int size, inputSize;
+    
+    public NeuronLayer(int numNeurons, int inputs) 
+    {
+        size = numNeurons;
+        layer = new Neuron[size];
+        inputSize = inputs;
+        initNeurons();
+    }
+
+public void changeInputAmount(int inputs) 
+{
+inputSize = inputs;
+initNeurons();
+}
+
+/*public double[] fire(double input[][])
+{
+
+}*/
+
+    private void initNeurons() 
+    {
+        for(int i = 0; i < layer.length; i++) 
+        {
+            layer[i] = new Neuron(inputSize);
+        }
+    }
+
+    
 
 }
+
