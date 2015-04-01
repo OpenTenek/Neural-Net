a3fff17b34db103e7d5316c943f2202ed2c67a1d
Thinic <hamilton.nick.1@gmail.com>
2015-04-01 10:49:09 -0500

    finish NeuralNetworkData for now

--- src/opentenek/ann/net/NeuralNetworkData.java
+++ src/opentenek/ann/net/NeuralNetworkData.java
@@ -14,8 +14,8 @@ package opentenek.ann.net;
  * manually.
  * 
  * This will also be useful for genetic algorithms
- * since it contains all the weights in a single
- * array.
+ * since it contains all the weights(and biases) 
+ * in a single array.
  */
 
 public class NeuralNetworkData
@@ -32,8 +32,8 @@ public class NeuralNetworkData
     // all the weights and bias in the neural 
     // network. To get a specific weight, you must
     // use this equation: 
-    //      a
-    //      ∑(N[i]W[i]) + n(W[a]+1) + w 
+    //     a-1
+    //      ∑ N[i](W[i]+1) + n(W[a]+1) + w 
     //     i=0
     // where N is the neurons array, 
     // W is the weights array,
@@ -48,11 +48,71 @@ public class NeuralNetworkData
     
     public NeuralNetworkData()
     {
-        layers = 1;
-        neurons = new int[]{ 1 };
-        weights = new int[]{ 1 };
+        this(1, new int[]{1}, new int[]{1});
+    }
+    
+    public NeuralNetworkData(int layers, int neurons[], int weights[]) 
+    {
+        this.layers = layers;
+        this.neurons = neurons;
+        this.weights = weights;
         
         isValid = initData();
+    }
+    
+    public NeuralNetworkData(int layers, int neurons[], int weights[], double data[]) 
+    {
+        this.layers = layers;
+        this.neurons = neurons;
+        this.weights = weights;
+        
+        isValid = initData(data);
+    }
+    
+    public boolean isValid() { return isValid; }
+    
+    public double getWeight(int layer, int neuron, int weight) 
+    {
+        if(!valid(layer, neuron, weight)) return 0.0;
+        
+        return data[getIndex(layer, neuron, weight)];
+    }
+    
+    public boolean setWeight(int layer, int neuron, int weight, double value) 
+    {
+        if(!valid(layer, neuron, weight)) return false;
+        
+        data[getIndex(layer, neuron, weight)] = value;
+        
+        return true;
+    }
+    
+    private int getIndex(int layer, int neuron, int weight) 
+    {
+        int index = 0;
+        for(int i = 0; i < layer; i++) 
+            index += neurons[i] * (weights[i] + 1);
+        index += neuron * (weights[layer] + 1);
+        index += weight;
+        
+        return index;
+    }
+    
+    // checks if in bounds, weights can include bias
+    private boolean inBounds(int layer, int neuron, int weight) 
+    {
+        if(layer < 0 || layer >= layers) return false;
+        if(neuron < 0 || neuron >= neurons[layer]) return false;
+        // only greater than, since bias is weights.length
+        // position
+        if(weight < 0 || weight > weights[layer]) return false;
+        
+        return true;
+    }
+    
+    private boolean valid(int layer, int neuron, int weight) 
+    {
+        return isValid && inBounds(layer, neuron, weight);
     }
     
     private boolean initData() 
@@ -63,11 +123,49 @@ public class NeuralNetworkData
         int size = 0;
         for(int i = 0; i < layers; i++) 
         {
-            
+            size += neurons[i] * (weights[i]+1);
         }
+        data = new double[size];
         
         return true;
     }
     
+    private boolean initData(double data[]) 
+    {
+        if(neurons.length != layers || weights.length != layers) 
+            return false;
+        
+        int size = 0;
+        for(int i = 0; i < layers; i++) 
+        {
+            size += neurons[i] * (weights[i]+1);
+        }
+        if(data.length != size) return false;
+        this.data = data;
+        
+        return true;
+    }
     
+    public String toString() 
+    {
+        String str = "";
+        
+        for(int layer = 0; layer < layers; layer++) 
+        {
+            str += "|";
+            
+            for(int neuron = 0; neuron < neurons[layer]; neuron++) 
+            {
+                for(int weight = 0; weight < weights[layer]; weight++) 
+                {
+                    str += getWeight(layer, neuron, weight) + ",";
+                }
+                str += "(" + getWeight(layer, neuron, weights[layer]) + ")";
+                str += "|";
+            }
+            str += "\n";
+        }
+        
+        return str;
+    }
 }
