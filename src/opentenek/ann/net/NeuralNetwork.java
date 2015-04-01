72d07526c2d9f7116e6cae981fae4251405feb1c
Thinic <hamilton.nick.1@gmail.com>
2015-03-31 20:51:29 -0500

    update comment header

--- src/opentenek/ann/net/NeuralNetwork.java
+++ src/opentenek/ann/net/NeuralNetwork.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 /* 
  * File Name : NeuronNetwork.java
- * Author(s) : Thinic
+ * Author(s) : Thinic, Ike
  * Created   : 2015 Mar 31
  * Updated   : 2015 Mar 31
  * Version   : 0.1.2
--- src/opentenek/ann/net/NeuronLayer.java
+++ src/opentenek/ann/net/NeuronLayer.java
@@ -2,7 +2,7 @@ package opentenek.ann.net;
 
 /* 
  * File Name : NeuronLayer.java
- * Author(s) : Ike, Thinic
+ * Author(s) : Thinic, Ike
  * Created   : 2015 Mar 31
  * Updated   : 2015 Mar 31
  * Version   : 0.1.2
