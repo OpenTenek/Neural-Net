854ae7c36fc012f120fd8c81a5878743e298606b
Thinic <hamilton.nick.1@gmail.com>
2015-04-01 14:39:58 -0500

    add GAMain.java

---
+++ src/opentenek/ann/ga/GAMain.java
@@ -0,0 +1,19 @@
+package opentenek.ann.ga;
+
+/* 
+ * File Name : GAMain.java
+ * Author(s) : Thinic
+ * Created   : 2015 Apr 1
+ * Updated   : 2015 Apr 1
+ * Version   : 0.0.1
+ * 
+ * Main method for genetic algorithm practice.
+ */
+
+public class GAMain
+{
+    public static void main(String args[]) 
+    {
+        
+    }
+}
