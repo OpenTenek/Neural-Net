d7daf4b98c1c9bdf8b5a2b9a50db2b516ca584ef
Thinic <hamilton.nick.1@gmail.com>
2015-04-05 11:04:10 -0500

    add comment

--- src/opentenek/ann/graphics/GraphImage.java
+++ src/opentenek/ann/graphics/GraphImage.java
@@ -37,6 +37,7 @@ public class GraphImage
         return image;
     }
     
+    // draws every point for now
     private void redraw() 
     {
         Graphics2D g = (Graphics2D) image.getGraphics();
